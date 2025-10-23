e65aafdcfb937038b71ace3bf7ca81884deaaff0
Luvys99 <152071667+Luvys99@users.noreply.github.com>
2025-10-23 17:18:12 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d\n", num1 + num2);
+	printf("%d\n", num1 - num2);
+	printf("%d\n", num1 * num2);
+	printf("%d\n", num1 / num2);
+	printf("%d\n", num1 % num2);
+
+}
