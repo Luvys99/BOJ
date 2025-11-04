ed27dcb34f85b8c18ce83797509d37eb0e1c4f85
Luvys99 <152071667+Luvys99@users.noreply.github.com>
2025-11-04 12:47:22 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.cc
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -1,31 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	// 입력받은 문자열과 찾으려고 하는 문자열을 입력받고 몇 번 등장하는지 세는 변수 선언
-	// 중복되어 세는 것은 제외 = 문자열의 위치 상관없이 찾을 수 있지만 겹쳐서 중복적으로 문자열을 찾는 것을 제외시킨다면
-	// 처음 찾는 문자열을 발견하고 읽은 뒤 부터 다시 문자열을 세기만 하면 될듯
-
-	string str;
-	string word;
-
-	getline(cin, str);
-	getline(cin, word);
-
-	int count = 0;
-	size_t current_pos = str.find(word);
-
-	while (current_pos != string::npos)
-	{
-		count++;
-		current_pos = str.find(word, current_pos + word.size());
-	}
 	
-	cout << count << '\n';
-	return 0;
+	char str[2502] = { 0 };
+	char ans[52] = { 0 };
 
+	fgets(str, sizeof(str), stdin);
+	str[strcspn(str, "\n")] = '\0';
+
+	fgets(ans, sizeof(ans), stdin);
+	ans[strcspn(ans, "\n")] = '\0';
+
+	int len = strlen(ans);
+	int Count = 0;
+
+	if (len == 0)
+	{
+		printf("0\n");
+		return 0;
+	}
+
+	char* current_pos = str;
+	char* found;
+
+	while ((found = strstr(current_pos, ans)) != NULL)
+	{
+		Count++;
+		current_pos = found + len;
+	}
+
+	printf("%d\n", Count);
+	return 0;
 }
