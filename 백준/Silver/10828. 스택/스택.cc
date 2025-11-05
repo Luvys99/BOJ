6f38a48c6793719ee6481b37707a52df52a37644
Luvys99 <152071667+Luvys99@users.noreply.github.com>
2025-11-05 15:59:26 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,70 +1,71 @@
-#include <bits/stdc++.h>
-using namespace std;
+#define MX 100000
+#include <stdio.h>
+#include <string.h>
+
+int dat[MX];
+int pos = 0;
 
 int main()
 {
-	cin.tie(0);
-	ios_base::sync_with_stdio(NULL);
-	stack<int> S;
+	
+	int num;
+	scanf("%d", &num);
 
-	//cin도 개행문자까지 읽어드려서 버퍼를 비우기
-	int num = 0;
-	cin >> num;
-	cin.ignore();
-
-	string input;
-	while (num > 0)
+	while (num-- > 0)
 	{
-		//공백있는 문자열 입력받기
-		getline(cin, input);
-		//C++ 표준으로 문자열을 읽거나 쓸 때 편리한 방법을 제공하고 문자열로부터 값을 추출하거나 문자열에 값을 삽입할 때 사용
-		stringstream ss(input);
-
-		//문자열로 받은 input string 에서 공백까지 문자열로 받음
-		string command;
-		ss >> command;
+		char str[100];
+		// 문자열은 크기 인자 필수로 넣어야됨 (unsigned int)로
+		// %c를 제외한 %s, %d, %f는 입력전 개행문자, 공백을 무시하기 때문에 개행문자 안 없애도 됨
+		scanf("%s", str);
 		
-		// 스택에 값만 push함
-		if (command == "push")
+		// C언어는 무조건 strcmp 문자열 비교로
+		if (strcmp(str, "push") == 0)
 		{
-			//문자열로 받은 input string에서 공백까지 int 값으로 추출함
-			int argument;
-			ss >> argument;
-
-			S.push(argument);
+			int element;
+			scanf("%d", &element);
+			dat[pos++] = element;
 		}
-		// 원래 stack.pop 함수는 스택에서 값만 삭제함 스택이 비어있는데 사용하면 런타임 에러발생
-		else if (command == "pop")
+		else if (strcmp(str, "pop") == 0)
 		{
-			if (S.empty() == 0)
+			//empty를 대신해서 pos가 0일 때
+			if (pos == 0)
 			{
-				cout << S.top() << "\n";
-				S.pop();
+				printf("-1\n");
 			}
 			else
 			{
-				cout << -1 << "\n";
+				//pos 값은 top 원소의 +1이기 때문에
+				printf("%d\n", dat[pos - 1]);
+				pos--;
 			}
 		}
-		// 스택 안에 요소만큼 정수 값을 반환
-		else if (command == "size")
+		else if (strcmp(str, "size") == 0)
 		{
-			cout << S.size() << "\n";
-			
+			printf("%d\n", pos);
 		}
-		//비어 있으면 1을 반환하고 채워져있으면 0을 반환함
-		else if (command == "empty")
+		else if (strcmp(str, "empty") == 0)
 		{
-			cout << S.empty() << "\n";
+			if (pos == 0)
+			{
+				printf("1\n");
+			}
+			else
+			{
+				printf("0\n");
+			}
 		}
-		// 스택의 최상단 요소를 출력하는 함수로 얘도 스택이 비어져 있는데 사용하면 런타임 에러 발생
-		else if (command == "top")
+		else if (strcmp(str, "top") == 0)
 		{
-			if (S.empty() == 0) cout << S.top() << "\n";
-			else cout << -1 << "\n";
+			if (pos == 0)
+			{
+				printf("-1\n");
+			}
+			else
+			{
+				//pos 값은 top 원소의 +1이기 때문에
+				printf("%d\n", dat[pos - 1]);
+			}
 		}
-		num--;
 	}
 
-	return 0;
 }
