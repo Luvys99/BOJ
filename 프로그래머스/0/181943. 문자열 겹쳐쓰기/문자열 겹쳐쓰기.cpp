66beb428a9250c91186e8b5e2bc04b0a2b234025
Luvys99 <152071667+Luvys99@users.noreply.github.com>
2026-04-18 15:22:27 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    my_string.replace(s, overwrite_string.size(), overwrite_string);
+    answer = my_string;
+    return answer;
+}
